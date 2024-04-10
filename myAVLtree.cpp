aaa6a9f5cb2312eecc8bb0b9206209e91c6c7d2f
nabhant <nt142003@gmail.com>
2024-04-10 12:32:56 -0400

    uosfdhiofds

--- myAVLtree.cpp
+++ myAVLtree.cpp
@@ -3,26 +3,73 @@
 #include <iostream>
 
 void treeMedian(const std::vector<int>* instructions) {
-    AvlTree<int> tree;
+    AvlTree small;
+    AvlTree large;
+
     std::vector<int> medians;
 
-    for (int instruction : *instructions) {
-        if (instruction >= 0) {
-            tree.insert(instruction);
-        } else {
-            if (!tree.isEmpty()) {
-                // Assuming the definition of getMedian() method in AvlTree
-                // This method would need to be implemented as part of AvlTree
-                int median = tree.getMedian();
-                medians.push_back(median);
-                tree.remove(median);
+    auto start = std::chrono::high_resolution_clock::now();
+
+    int small_size = 0, large_size = 0;
+
+    for (auto it = instructions->begin(); it != instructions->end(); ++it)
+    {
+        if (*it != -1)
+        {
+            if (small.isEmpty() || *it <= small.getMaximum())
+            {
+                small.insert(*it);
+                small_size++;
+            }
+            else
+            {
+                large.insert(*it);
+                large_size++;
+            }
+
+            while (small_size > large_size + 1)
+            {
+                large.insert(small.getMaximum());
+                large_size++;
+
+                small.remove(small.getMaximum());
+                small_size--;
+            }
+
+            while (large_size > small_size)
+            {
+                small.insert(large.getMinimum());
+                small_size++;
+
+                large.remove(large.getMinimum());
+                large_size--;
+            }
+        }
+        else
+        {
+            medians.push_back(small.getMaximum());
+            small.remove(small.getMaximum());
+            small_size--;
+
+            while (small_size < large_size)
+            {
+                small.insert(large.getMinimum());
+                small_size++;
+
+                large.remove(large.getMinimum());
+                large_size--;
             }
         }
     }
 
-    // Print medians
-    for (int median : medians) {
-        std::cout << median << ' ';
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+
+    for (int median : medians)
+    {
+        std::cout << median << " ";
     }
-    std::cout << std::endl;
+    std::cout << "\n";
+
+    // std::cout << "Algorithm duration: " << duration.count() << "ms\n";
 }
